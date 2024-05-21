0e2b757a4d2a3fad2b9ce0f668a62eca1375b0f7
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-05-21 16:18:11 +0200

    Made all functions vectorized

--- utils/my_skellam.R
+++ utils/my_skellam.R
@@ -3,56 +3,21 @@ my_dskellam <- function(x, lambda1, lambda2) {
   return(exp(-(lambda1+lambda2))*(lambda1/lambda2)^{x/2}*besselI(x=2*sqrt(lambda1*lambda2),nu = abs(x)))
 }
 
-my_dzeroinflatedskellam <- function(x, lambda1, lambda2,p) {
-  x=round(x) # to manage the case x is not integer
-  prob=(1-p)*exp(-(lambda1+lambda2))*(lambda1/lambda2)^{x/2}*besselI(x=2*sqrt(lambda1*lambda2),nu = abs(x))
-  if(x==0){
-    prob=prob+p
-  }
+my_dzeroinflatedskellam <- function(x, lambda1, lambda2, p) {
+  # Ensure x is a vector of integers
+  x <- round(x)
+  # Calculate the probabilities for each element in x, lambda1, lambda2, and p
+  prob <- (1 - p) * exp(-(lambda1 + lambda2)) * (lambda1 / lambda2)^(x / 2) * besselI(x = 2 * sqrt(lambda1 * lambda2), nu = abs(x))
+  # If x is zero, add p to the probability
+  prob[x == 0] <- prob[x == 0] + p
   return(prob)
 }
 
-# my_rskellam<- function(n,lambda1,lambda2){
-#   samples<-numeric(0)
-#   while (length(samples)<n) {
-#     x_star<- rnorm(n=1,mean =(lambda1-lambda2),sd=sqrt(lambda1+lambda2))
-#     acc_prob<- my_dskellam(x_star,lambda1,lambda2)/dnorm(x_star,mean =(lambda1-lambda2),sd=sqrt(lambda1+lambda2))
-#     if (runif(1) < acc_prob) {
-#       samples <- c(samples, round(x_star))
-#     }
-#   }
-#   return(samples)
-# }
-
-# my_rzeroinflatedskellam<- function(n,lambda1,lambda2,p){
-#   samples<-numeric(0)
-#   while (length(samples)<n) {
-#     x_star<- rnorm(n=1,mean =(lambda1-lambda2),sd=sqrt(lambda1+lambda2))
-#     acc_prob<- my_dzeroinflatedskellam(x_star,lambda1,lambda2,p)/dnorm(x_star,mean =(lambda1-lambda2),sd=sqrt(lambda1+lambda2))
-#     if (runif(1) < acc_prob) {
-#       samples <- c(samples, round(x_star))
-#     }
-#   }
-#   return(samples)
-# }
-library(skellam)
-# NB: rskallam internally computes 2 rpois, and I don't want this
-#     but still I can exploit the function qskellam without re-inventing the wheel
 my_rskellam<- function(n,lambda1,lambda2){
   # Inverse sampling
   q= runif(n,min=0,max=1)
   return(qskellam(q,lambda1,lambda2))
 }
-
-# Test that it works:
-# simulated_data= my_rskellam(10000,4,2)
-# plot(table(simulated_data)/length(simulated_data),xlab = "values",ylab="density")
-# # Overlay theoretical PMF plot
-# success <- min(simulated_data):max(simulated_data)
-# lines(success+0.1, dskellam(success,4,2), type = 'h', col = 'red', lwd = 2, lty = 2)
-# legend("topright", legend = c("Empirical densities", "Theoretical densities"), lty = c(1,2), 
-#        col = c("black", "red"), bty = "n")
-# title("Validity check of my skellam rng")
 
 my_rzeroinflatedskellam<- function(n,lambda1,lambda2,p){
   samples<- vector(mode="numeric",length = n)
@@ -66,6 +31,3 @@ my_rzeroinflatedskellam<- function(n,lambda1,lambda2,p){
   }
   return(samples)
 }
-
-
-
