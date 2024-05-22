f14ac5ec88a5a59f20af31af4c29b940f5f0e81e
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-05-22 19:19:26 +0200

    Updated FinalRankingSimulation: added comparison btw offline and online models

--- FinalRankingSimulation.R
+++ FinalRankingSimulation.R
@@ -6,7 +6,9 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(bayesplot)
+library(DescTools)
 source("utils/my_skellam.R")
+source("utils/final_pts_distribution.R")
 #-------------------------------------------------------------------------------
 # Global settings and variables
 #-------------------------------------------------------------------------------
@@ -16,7 +18,8 @@ N_WARMUP=1000
 DATA_DIR= "data/"
 STAN_DIR= "stan/"
 SEASON="2122"
-MODELS_DIR= paste0("estimated_models/season_",SEASON,"/online_models/")
+ONLINE_MODELS_DIR= paste0("estimated_models/season_",SEASON,"/online_models/")
+OFFLINE_MODELS_DIR= paste0("estimated_models/season_",SEASON,"/models/")
 #-------------------------------------------------------------------------------
 # Data import and preparation
 #-------------------------------------------------------------------------------
@@ -35,82 +38,65 @@ teams<- str_replace_all(teams, " ", "")
 #-------------------------------------------------------------------------------
 # Rankings Simulation
 #-------------------------------------------------------------------------------
-# Rankings after the first half of the league 
-# Note: this df is s.t. each column is associated to a team
-#       nrows is N_CHAINS*(N_ITERS-N_WARMUP) to be friendly with the MCMC posteriors
-teams_pts <- data.frame(matrix(NA,nrow = N_CHAINS*(N_ITERS-N_WARMUP),ncol=n_teams))
-colnames(teams_pts)=teams
-for (t in 1:n_teams){
-  n_wins= SerieA_data[1:190,] %>% filter(((ht==t) &(FTHG>FTAG)) | ((at==t) & (FTHG<FTAG)))%>% nrow() 
-  n_draws= SerieA_data[1:190,] %>% filter(((ht==t) | (at==t)) & (FTHG == FTAG))%>% nrow() 
-  teams_pts[,t]= 3*n_wins+n_draws
+
+# 1) Offline models
+offline_pts_distribution<- final_pts_distribution(teams,start=20,end=38,
+                                                  n_chains=N_CHAINS,n_iters=N_ITERS,n_warmup=N_WARMUP,
+                                                  models_dir =OFFLINE_MODELS_DIR)
+offline_final_table<- data.frame(Team= teams,
+                         Pts_mean=NA,
+                         Pts_mode=NA,
+                         Pts_lower.95=NA,
+                         Pts_lower.50=NA,
+                         Pts_upper.50=NA,
+                         Pts_upper.95=NA)
+for(t in 1:n_teams){
+  offline_final_table$Pts_mean[t]<- offline_pts_distribution[,t] %>% mean() %>% round()
+  offline_final_table$Pts_mode[t]<- offline_pts_distribution[,t] %>% DescTools::Mode() %>% Mean() %>% round() #We manage the multimodal case by taking their mean
+  offline_final_table$Pts_lower.95[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.025) %>% round()
+  offline_final_table$Pts_lower.50[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.25) %>% round()
+  offline_final_table$Pts_upper.50[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.75) %>% round()
+  offline_final_table$Pts_upper.95[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.975) %>% round()
 }
 
-# Iteration over the 2nd half
-for (m in 20:n_matchdays){
-  cat(paste0("...Simulation of matchday n. ",m,"...\n"))
-  #-------------------------------------------------
-  # (1) Get the current matchday to predict
-  test_set=SerieA_data[(10*m -9):(10*m),]
-  test_set=na.omit(test_set) #just to manage postponed matches in last batch
-  #-------------------------------------------------
-  # (2) Load the most recent model and some of its stuff
-  cat("...Loading the model and retrieving useful info...\n")
-  load(paste0(MODELS_DIR,"matchday",m-1,"/KN_matchday",m-1,".rds"))
-  posterior<- as.array(KN_model)
-  N_ITERS=dim(posterior)[1]
-  N_CHAINS=dim(posterior)[2]
-  mu = posterior[,,"mu"]
-  home=posterior[,,"home_advantage"]
-  p = posterior[,,"p"]
-  #-------------------------------------------------
-  # (3) Make predictions
-  cat("...Computing predictions and updating rankings...\n")
-  for(mm in 1:nrow(test_set)){
-    ht=test_set$ht[mm]
-    at=test_set$at[mm]
-    attH=posterior[,,paste0("att[",ht,"]")]
-    defH=posterior[,,paste0("def[",ht,"]")]
-    attA=posterior[,,paste0("att[",at,"]")]
-    defA=posterior[,,paste0("def[",at,"]")]
-    theta_H = exp(mu+home+attH+defA)
-    theta_A = exp(mu+attA+defH)
-    GD<- mapply(my_rzeroinflatedskellam, 1,theta_H, theta_A,p)
-    
-    # (4) Calculate points according to predicted goal diffs
-    pts_ht = ifelse(GD > 0, 3, ifelse(GD < 0, 0, 1))
-    pts_at = ifelse(GD < 0, 3, ifelse(GD > 0, 0, 1))
-    # (5) Sum them to the previous points
-    teams_pts[,ht] = teams_pts[,ht]+pts_ht
-    teams_pts[,at] = teams_pts[,at]+pts_at
-    #-------------------------------------------------
-  }
-  cat("-------------------------------------------------\n")
-}
-# Now teams_pts has a distribution of final points for each team
-final_table<- data.frame(Team= teams,
-                         Pts_mean=NA,
-                         Pts_lower=NA,
-                         Pts_upper=NA)
+# 2) Online models
+online_pts_distribution<- final_pts_distribution(teams,start=20,end=38,
+                                                 n_chains=N_CHAINS,n_iters=N_ITERS,n_warmup=N_WARMUP,
+                                                 models_dir =ONLINE_MODELS_DIR)
+online_final_table<- data.frame(Team= teams,
+                                 Pts_mean=NA,
+                                 Pts_mode=NA,
+                                 Pts_lower.95=NA,
+                                 Pts_lower.50=NA,
+                                 Pts_upper.50=NA,
+                                 Pts_upper.95=NA)
 for(t in 1:n_teams){
-  final_table$Pts_mean[t]<- teams_pts[,t] %>% mean() %>% round()
-  final_table$Pts_lower[t]<- teams_pts[,t] %>% quantile(probs = 0.025) %>% round()
-  final_table$Pts_upper[t]<- teams_pts[,t] %>% quantile(probs = 0.975) %>% round()
+  online_final_table$Pts_mean[t]<- online_pts_distribution[,t] %>% mean() %>% round()
+  online_final_table$Pts_mode[t]<- online_pts_distribution[,t] %>% DescTools::Mode() %>% Mean() %>% round() #We manage the multimodal case by taking their mean
+  online_final_table$Pts_lower.95[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.025) %>% round()
+  online_final_table$Pts_lower.50[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.25) %>% round()
+  online_final_table$Pts_upper.50[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.75) %>% round()
+  online_final_table$Pts_upper.95[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.975) %>% round()
 }
 
 # To view directly the dataframe
-View(final_table)
+View(offline_final_table %>% arrange(desc(Pts_mode)))
+View(online_final_table %>% arrange(desc(Pts_mode)))
+
+
+
 # Plot of the points distribution for each team
 color_scheme_set("brightblue")
 # Lazy passages to re-order columns basing on their mean
-team_means <- teams_pts %>%
+ordered_teams<- online_pts_distribution %>%
   summarise_all(mean) %>%
   round() %>%
   gather() %>%
   arrange(desc(value)) %>%
   pull(key)
 # Reorder columns based on mean points
-teams_pts <- teams_pts[, team_means]
+offline_pts_distribution <- offline_pts_distribution[, ordered_teams]
+online_pts_distribution <- online_pts_distribution[, ordered_teams]
 # Compute the real points in order to add them in the mcmc_plot
 real_points= data.frame(
   Team=teams,
@@ -123,21 +109,22 @@ for (t in 1:n_teams){
 }
 
 # Mcmc plot of the points distribution
-mcmc_intervals(teams_pts)+
-  labs(title="Estimated final rankings",
-       x = "Points",
-       y= "Teams")+
-  geom_point(
-    data = real_points,
-    aes(x = Pts, y = Team), # Map color to a variable
-    size = 3.5,
-    shape=21,
-    color="black",
-    fill="indianred2"
-  )+
-  theme(plot.title = element_text(hjust = 0.5,face = "bold",size=18),
-        axis.title.x = element_text(size=12, face="bold", colour = "black"),
-        axis.text.x = element_text(size=10, face="plain", colour = "black"),
-        axis.title.y = element_text(size=12, face="bold", colour = "black"),
-        axis.text.y = element_text(size=10, face="plain", colour = "black"),
-  )
+online_pts_distribution %>% 
+  mcmc_intervals()+
+    labs(title="Estimated final rankings",
+         x = "Points",
+         y= "Teams")+
+    geom_point(
+      data = real_points,
+      aes(x = Pts, y = Team), 
+      size = 3.5,
+      shape=21,
+      color="black",
+      fill="indianred2"
+    )+
+    theme(plot.title = element_text(hjust = 0.5,face = "bold",size=18),
+          axis.title.x = element_text(size=12, face="bold", colour = "black"),
+          axis.text.x = element_text(size=10, face="plain", colour = "black"),
+          axis.title.y = element_text(size=12, face="bold", colour = "black"),
+          axis.text.y = element_text(size=10, face="plain", colour = "black"),
+    )
