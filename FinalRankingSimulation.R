716b2be49da45c7866245ca70208bd33107ab204
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2024-05-24 18:10:32 +0200

    Final commit (?)

--- FinalRankingSimulation.R
+++ FinalRankingSimulation.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(ggplot2)
 library(bayesplot)
 library(DescTools)
+library(knitr)
+library(kableExtra)
 source("utils/my_skellam.R")
 source("utils/final_pts_distribution.R")
 #-------------------------------------------------------------------------------
@@ -40,65 +42,110 @@ teams<- str_replace_all(teams, " ", "")
 #-------------------------------------------------------------------------------
 
 # 1) Offline models
-offline_pts_distribution<- final_pts_distribution(teams,start=20,end=38,
-                                                  n_chains=N_CHAINS,n_iters=N_ITERS,n_warmup=N_WARMUP,
-                                                  models_dir =OFFLINE_MODELS_DIR)
-write.csv(offline_pts_distribution,"report/final_pts_offline.csv",row.names = FALSE)
-offline_final_table<- data.frame(Team= teams,
-                         Pts_mean=NA,
-                         Pts_mode=NA,
-                         Pts_lower.95=NA,
-                         Pts_lower.50=NA,
-                         Pts_upper.50=NA,
-                         Pts_upper.95=NA)
+final_pts_offline<- final_pts_distribution(teams,start=20,end=38,
+                                           n_chains=N_CHAINS,
+                                           n_iters=N_ITERS,
+                                           n_warmup=N_WARMUP,
+                                           models_dir =OFFLINE_MODELS_DIR)
+
+final_pts_online<- final_pts_distribution(teams,start=20,end=38,
+                                          n_chains=N_CHAINS,
+                                          n_iters=N_ITERS,
+                                          n_warmup=N_WARMUP,
+                                          models_dir =ONLINE_MODELS_DIR)
+
+
+write.csv(final_pts_offline,"report/final_pts_offline.csv",row.names = FALSE)
+write.csv(final_pts_online,"report/final_pts_online.csv",row.names = FALSE)
+
+
+offline_rankings<- data.frame(Team= teams,
+                              Pts_mean=NA,
+                              Pts_mode=NA,
+                              Pts_lower.95=NA,
+                              Pts_lower.50=NA,
+                              Pts_upper.50=NA,
+                              Pts_upper.95=NA)
+
+online_rankings<- data.frame(Team= teams,
+                             Pts_mean=NA,
+                             Pts_mode=NA,
+                             Pts_lower.95=NA,
+                             Pts_lower.50=NA,
+                             Pts_upper.50=NA,
+                             Pts_upper.95=NA)
+
 for(t in 1:n_teams){
-  offline_final_table$Pts_mean[t]<- offline_pts_distribution[,t] %>% mean() %>% round()
-  offline_final_table$Pts_mode[t]<- offline_pts_distribution[,t] %>% DescTools::Mode() %>% Mean() %>% round() #We manage the multimodal case by taking their mean
-  offline_final_table$Pts_lower.95[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.025) %>% round()
-  offline_final_table$Pts_lower.50[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.25) %>% round()
-  offline_final_table$Pts_upper.50[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.75) %>% round()
-  offline_final_table$Pts_upper.95[t]<- offline_pts_distribution[,t] %>% quantile(probs = 0.975) %>% round()
+  
+  # Update rankings for offline model
+  offline_rankings$Pts_mean[t]<- final_pts_offline[,t] %>% mean() %>% round()
+  offline_rankings$Pts_mode[t]<- final_pts_offline[,t] %>% DescTools::Mode() %>% mean() %>% round() #mean just to manage multimodal case
+  offline_rankings$Pts_lower.95[t]<- final_pts_offline[,t] %>% quantile(probs = 0.025) %>% round()
+  offline_rankings$Pts_lower.50[t]<- final_pts_offline[,t] %>% quantile(probs = 0.25) %>% round()
+  offline_rankings$Pts_upper.50[t]<- final_pts_offline[,t] %>% quantile(probs = 0.75) %>% round()
+  offline_rankings$Pts_upper.95[t]<- final_pts_offline[,t] %>% quantile(probs = 0.975) %>% round()
+  
+  # Update rankings for online model
+  online_rankings$Pts_mean[t]<- final_pts_online[,t] %>% mean() %>% round()
+  online_rankings$Pts_mode[t]<- final_pts_online[,t] %>% DescTools::Mode() %>% mean() %>% round() #mean just to manage multimodal case
+  online_rankings$Pts_lower.95[t]<- final_pts_online[,t] %>% quantile(probs = 0.025) %>% round()
+  online_rankings$Pts_lower.50[t]<- final_pts_online[,t] %>% quantile(probs = 0.25) %>% round()
+  online_rankings$Pts_upper.50[t]<- final_pts_online[,t] %>% quantile(probs = 0.75) %>% round()
+  online_rankings$Pts_upper.95[t]<- final_pts_online[,t] %>% quantile(probs = 0.975) %>% round()
 }
 
-# 2) Online models
-online_pts_distribution<- final_pts_distribution(teams,start=20,end=38,
-                                                 n_chains=N_CHAINS,n_iters=N_ITERS,n_warmup=N_WARMUP,
-                                                 models_dir =ONLINE_MODELS_DIR)
-write.csv(online_pts_distribution,"report/final_pts_online.csv",row.names = FALSE)
-online_final_table<- data.frame(Team= teams,
-                                 Pts_mean=NA,
-                                 Pts_mode=NA,
-                                 Pts_lower.95=NA,
-                                 Pts_lower.50=NA,
-                                 Pts_upper.50=NA,
-                                 Pts_upper.95=NA)
-for(t in 1:n_teams){
-  online_final_table$Pts_mean[t]<- online_pts_distribution[,t] %>% mean() %>% round()
-  online_final_table$Pts_mode[t]<- online_pts_distribution[,t] %>% DescTools::Mode() %>% Mean() %>% round() #We manage the multimodal case by taking their mean
-  online_final_table$Pts_lower.95[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.025) %>% round()
-  online_final_table$Pts_lower.50[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.25) %>% round()
-  online_final_table$Pts_upper.50[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.75) %>% round()
-  online_final_table$Pts_upper.95[t]<- online_pts_distribution[,t] %>% quantile(probs = 0.975) %>% round()
-}
 
-# To view directly the dataframe
-View(offline_final_table %>% arrange(desc(Pts_mode)))
-View(online_final_table %>% arrange(desc(Pts_mode)))
+# Simulated Rankings at the end of the season
+table_offline<- offline_rankings %>% 
+  arrange(desc(Pts_mode)) %>%
+  mutate(Position = row_number()) %>%
+  select(Position,Team,Pts_mean,Pts_mode,Pts_lower.95,Pts_lower.50,Pts_upper.50,Pts_upper.95) %>%
+  kbl(caption = "Serie A 2021-2022 simulated rankings (offline models)", align = 'c') %>%
+  kable_styling(font_size = 12, full_width = F) %>%
+  row_spec(0, background = "#FFFFFF", color = "#000000",bold=T) %>%
+  # Champions League
+  row_spec(1:4, background = "#8AD1F3") %>% 
+  # Europa League
+  row_spec(5:6, background = "#EEA256") %>%
+  # Conference League
+  row_spec(7, background = "#E1D625") %>%  
+  # Relegation
+  row_spec((nrow(offline_rankings)-2):nrow(offline_rankings), background = "#FF9E94") %>%
+  row_spec(1:nrow(offline_rankings), extra_css = "border-top: 1px solid black;")
 
+
+table_online<- online_rankings %>% 
+  arrange(desc(Pts_mode)) %>%
+  mutate(Position = row_number()) %>%
+  select(Position,Team,Pts_mean,Pts_mode,Pts_lower.95,Pts_lower.50,Pts_upper.50,Pts_upper.95) %>%
+  kbl(caption = "Serie A 2021-2022 simulated rankings (online models)", align = 'c') %>%
+  kable_styling(font_size = 12, full_width = F) %>%
+  row_spec(0, background = "#FFFFFF", color = "#000000",bold=T) %>%
+  # Champions League
+  row_spec(1:4, background = "#8AD1F3") %>% 
+  # Europa League
+  row_spec(5:6, background = "#EEA256") %>%
+  # Conference League
+  row_spec(7, background = "#E1D625") %>%  
+  # Relegation
+  row_spec((nrow(online_rankings)-2):nrow(online_rankings), background = "#FF9E94") %>%
+  row_spec(1:nrow(online_rankings), extra_css = "border-top: 1px solid black;")
 
 
 # Plot of the points distribution for each team
 color_scheme_set("brightblue")
 # Lazy passages to re-order columns basing on their mean
-ordered_teams<- online_pts_distribution %>%
+ordered_teams<- final_pts_offline %>%
   summarise_all(mean) %>%
   round() %>%
   gather() %>%
   arrange(desc(value)) %>%
   pull(key)
 # Reorder columns based on mean points
-offline_pts_distribution <- offline_pts_distribution[, ordered_teams]
-online_pts_distribution <- online_pts_distribution[, ordered_teams]
+final_pts_offline <- final_pts_offline[, ordered_teams]
+final_pts_online <- final_pts_online[, ordered_teams]
+
+
 # Compute the real points in order to add them in the mcmc_plot
 real_points= data.frame(
   Team=teams,
@@ -111,22 +158,43 @@ for (t in 1:n_teams){
 }
 
 # Mcmc plot of the points distribution
-online_pts_distribution %>% 
+final_pts_offline_plot<- final_pts_offline %>% 
   mcmc_intervals()+
-    labs(title="Estimated final rankings",
-         x = "Points",
-         y= "Teams")+
-    geom_point(
-      data = real_points,
-      aes(x = Pts, y = Team), 
-      size = 3.5,
-      shape=21,
-      color="black",
-      fill="indianred2"
-    )+
-    theme(plot.title = element_text(hjust = 0.5,face = "bold",size=18),
-          axis.title.x = element_text(size=12, face="bold", colour = "black"),
-          axis.text.x = element_text(size=10, face="plain", colour = "black"),
-          axis.title.y = element_text(size=12, face="bold", colour = "black"),
-          axis.text.y = element_text(size=10, face="plain", colour = "black"),
-    )
+  labs(title="Estimated final rankings (offline model)",
+       x = "Points",
+       y= "Teams")+
+  geom_point(
+    data = real_points,
+    aes(x = Pts, y = Team), 
+    size = 3.5,
+    shape=21,
+    color="black",
+    fill="indianred2"
+  )+
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size=18),
+        axis.title.x = element_text(size=12, face="bold", colour = "black"),
+        axis.text.x = element_text(size=10, face="plain", colour = "black"),
+        axis.title.y = element_text(size=12, face="bold", colour = "black"),
+        axis.text.y = element_text(size=10, face="plain", colour = "black"),
+  )
+
+final_pts_online_plot<- final_pts_online %>% 
+  mcmc_intervals()+
+  labs(title="Estimated final rankings (online model)",
+       x = "Points",
+       y= "Teams")+
+  geom_point(
+    data = real_points,
+    aes(x = Pts, y = Team), 
+    size = 3.5,
+    shape=21,
+    color="black",
+    fill="indianred2"
+  )+
+  theme(plot.title = element_text(hjust = 0.5,face = "bold",size=18),
+        axis.title.x = element_text(size=12, face="bold", colour = "black"),
+        axis.text.x = element_text(size=10, face="plain", colour = "black"),
+        axis.title.y = element_text(size=12, face="bold", colour = "black"),
+        axis.text.y = element_text(size=10, face="plain", colour = "black"),
+  )
+
---
+++ web-scraping/scraper.py
@@ -0,0 +1,83 @@
+#--------------------------------------------------------------------------------
+import os
+import sys
+import requests
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# Dictionary that maps league names to their respective codes for scraping
+LEAGUES = {
+    "PremierLeague" : "E0",
+    "Championship"  : "E1",
+    "SerieA"        : "I1",
+    "SerieB"        : "I2",
+    "LaLiga1"       : "SP1",
+    "LaLiga2"       : "SP2",
+    "Ligue1"        : "F1",
+    "Ligue2"        : "F2",
+    "Bundesliga1"   : "D1",
+    "Bundesliga2"   : "D2"
+}
+#--------------------------------------------------------------------------------
+
+# Class to scrape football data from football-data.co.uk
+#--------------------------------------------------------------------------------    
+class LeagueScraper():
+    def __init__(self, league, season):
+        """
+        Initialize LeagueScraper with league and season.
+        """
+        self.league = LEAGUES[league]
+        self.season = season
+        self.csv = None
+
+    def download(self):
+        """
+        Download CSV file from the specified URL.
+        """
+        url = f"https://www.football-data.co.uk/mmz4281/{self.season}/{self.league}.csv"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise HTTPError for bad responses
+            self.csv = response.text
+            print("File downloaded successfully.")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to download file: {e}")
+
+    def savecsv(self, filepath):
+        """
+        Save downloaded CSV data to a file.
+        """
+        if self.csv:
+            try:
+                # Check if file already exists
+                if os.path.exists(filepath):
+                    print("File already exists.")
+                    return
+
+                with open(filepath, "w") as f:
+                    f.write(self.csv)
+                print(f"File saved successfully at {filepath}")
+            except IOError as e:
+                print(f"Error saving file: {e}")
+        else:
+            print("No CSV to save, download it first.")
+
+if __name__ == "__main__":
+    # Check if season is provided from terminal(if not, 2324 will be used as default)
+    assert len(sys.argv) <= 2, "Usage: python3 scraper.py <season>"
+    season = sys.argv[1] if len(sys.argv) == 2 else "2324"
+    # Create directory to store data
+    if not os.path.exists(f"../data/season_{season}"):
+        os.makedirs(f"../data/season_{season}")
+    # Scrape data for all leagues
+    for league in LEAGUES.keys():
+        filepath = f"../data/season_{season}/{league}_{season}.csv" 
+        try:
+            scraper = LeagueScraper(league, season)
+            scraper.download()
+            scraper.savecsv(filepath)
+        except Exception as e:
+            print(f"Errors: {e}")
+            pass
+#--------------------------------------------------------------------------------
